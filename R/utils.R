8a66039d0acac0c59e7b94d30622f3a2f7619eab
Al J Abadi <al.jal.abadi@gmail.com>
2021-07-27 10:39:22 +1000

    export utils

--- R/utils.R
+++ R/utils.R
@@ -1,32 +1,53 @@
+#' Utilities
+#'
+#' Utility functions
+#'
+#' @param fmt,... passed to \code{\link[base]{sprintf}}
+#' @param filepath,path path to file
+#' @name utils
+#' @return None
+NULL
 ## ---- colourised 'cat's
+#' @examples
+#' cat2("this is a '%s' text", 'yellow')
+#' @rdname utils
+#' @export
 cat2 <- function(fmt, ...)
 { # a yellow cat
     cat("\033[38;5;214m", sprintf(fmt, ...), "\033[39m")
 }
 
-# cat2("this is a '%s' text", 'yellow')
-
+#' @examples
+#' cat3("this is a '%s' text", 'purple')
+#' @rdname utils
+#' @export
 cat3 <- function(fmt, ...)
 { # a purple cat
     cat("\033[35m", sprintf(fmt, ...), "\033[39m")
 }
 
-# cat3("this is a '%s' text", 'purple')
-
+#' @examples
+#' cat4("this is a '%s' text", 'blue')
+#' @rdname utils
+#' @export
 cat4 <- function(fmt, ...)
 { # a blue cat
     cat("\033[34m", sprintf(fmt, ...), "\033[39m")
 }
-
-# cat4("this is a '%s' text", 'blue')
 ## ---- END colourised 'cat's
-
+#' @examples
+#' rm_trailing("foo/bar/")
+#' # "foo/bar"
+#' @rdname utils
+#' @export
 ## remove possible trailing slash
 rm_trailing <- function(path)
 {
     gsub('/$', '', path)
 }
 
+#' @rdname utils
+#' @export
 file_exists <- function(filepath)
 {
     # cat that the file exists and run was skipped
@@ -36,8 +57,11 @@ file_exists <- function(filepath)
     cat2("\nUse --force 'TRUE' to overwrite.\n\n")
 }
 
+#' @rdname utils
+#' @export
 check_file_exists <- function(filepath)
 {
+    # if filepath does not exist, throw error
     if (!file.exists(filepath))
         stop(filepath, " does not exist\n")
     invisible(NULL)
